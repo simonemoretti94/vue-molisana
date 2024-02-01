90dd83265b5fc7e721bedf40fa1feef079b4dbfc
simonemoretti94 <simone.moretti.944@gmail.com>
2024-02-01 19:27:12 +0100

    3th adding items to DOM and styling

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,47 @@
+<script >
+
+export default {
+    name: 'ProductCard',
+    props: {
+        product: Object,
+        src: String,
+        text: String,
+    }
+}
+
+</script>
+
+
+<template>
+    <div class="product card my-4 mx-4">
+        <img :src="product.src" alt="">
+        <h6 class="text-primary text-center my-1">{{ product.titolo }}</h6>
+    </div>
+</template>
+
+
+<style scoped>
+.card {
+    border-bottom: 1.5px grey;
+    border-radius: 40px;
+
+
+    & img {
+        max-height: 150px;
+        border: solid 2px rgb(150, 150, 150);
+        box-shadow: 1px 2px rgb(0, 0, 0);
+        border-top-left-radius: 15px;
+        border-bottom-right-radius: 15px;
+    }
+
+    & h6 {
+        text-transform: capitalize;
+        font-style: italic;
+        text-shadow: 1px 1px rgb(88, 88, 88);
+    }
+}
+
+.card:hover {
+    filter: drop-shadow(4px 4px 2px rgb(168, 168, 168));
+}
+</style>
